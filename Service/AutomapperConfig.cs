c65c4263a4b5567a8fd0f42945f4e56854ffb542
Ayelen Palmieri <ayelenpalmieri9012@gmail.com>
2020-04-26 02:32:32 -0300

    Arquitectura Base

---
+++ Service/AutomapperConfig.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+//using Bussines.Entities;
+//using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class AutomapperConfig : Profile
+    {
+        public AutomapperConfig() {
+
+            //Data->Business
+
+            //CreateMap<Acceso, AccesoE>();
+            //CreateMap < CargaVehiculo, CargaVehiculoE>();
+            //CreateMap<Delegaciones, DelegacionesE>();
+            //CreateMap<Empleado, EmpleadoE>();
+            //CreateMap<Estacionamiento, EstacionamientoE>();
+            //CreateMap<User, UserE>();
+            //CreateMap<Usuario, UsuarioE>();
+            //CreateMap<Vehiculo, VehiculoE>();
+
+            ////Business->Data
+
+            //CreateMap<AccesoE, Acceso>();
+            //CreateMap<CargaVehiculoE, CargaVehiculo>();
+            //CreateMap<DelegacionesE, Delegaciones>();
+            //CreateMap<EmpleadoE, Empleado>();
+            //CreateMap<EstacionamientoE, Estacionamiento>();
+            //CreateMap<UserE, User>();
+            //CreateMap<UsuarioE, Usuario>();
+            //CreateMap<VehiculoE, Vehiculo>();
+        }
+    }
+}
